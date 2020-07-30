52e5103f5cf9a02af0c37cbe092254a69813bb24
Emiliana <38952746+emilianavt@users.noreply.github.com>
2020-07-30 22:57:56 +0200

    Updated default settings.

--- run.bat
+++ run.bat
@@ -8,6 +8,6 @@ set /p width=Select the width:
 set /p height=Select the height:
 set /p fps=Select the FPS:
 
-facetracker -c %cameraNum% -W %width% -H %height% -F %fps% -v 3 -P 1
+facetracker -c %cameraNum% -W %width% -H %height% -F %fps% -v 3 -P 1 --discard-after 0 --scan-every 0 --no-3d-adapt 1 --max-feature-updates 900
 
 pause
