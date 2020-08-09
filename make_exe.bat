cc7667734bf9c8ad6669fa8e15a3a91d5a178ca8
Emiliana <38952746+emilianavt@users.noreply.github.com>
2020-08-10 00:16:31 +0200

    Included `run.bat` to be copied in `make_exe.bat`.

--- make_exe.bat
+++ make_exe.bat
@@ -3,6 +3,7 @@ REM     --add-binary dshowcapture/dshowcapture_x86.dll;. ^
 REM     --add-binary dshowcapture/dshowcapture_x64.dll;. ^
 REM     --add-binary escapi/escapi_x86.dll;. ^
 REM     --add-binary escapi/escapi_x64.dll;. ^
+REM     --add-binary run.bat;. ^
 REM     facetracker.py
 
 pyinstaller --onedir ^
@@ -10,4 +11,5 @@ pyinstaller --onedir ^
     --add-binary dshowcapture/dshowcapture_x64.dll;. ^
     --add-binary escapi/escapi_x86.dll;. ^
     --add-binary escapi/escapi_x64.dll;. ^
+    --add-binary run.bat;. ^
     facetracker.py
