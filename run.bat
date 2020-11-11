733b957e0382d1eb8d3b210c82694286ee7ba5fc
Emiliana <38952746+emilianavt@users.noreply.github.com>
2020-11-11 15:42:56 +0100

    Updated dshowcapture and removed unnecessary questions from run.bat.

--- run.bat
+++ run.bat
@@ -9,10 +9,8 @@ set /p cameraNum=Select your camera from the list above and enter the correspond
 facetracker -a %cameraNum%
 
 set /p dcaps=Select your camera mode or -1 for default settings:
-set /p width=Select the width:
-set /p height=Select the height:
 set /p fps=Select the FPS:
 
-facetracker -c %cameraNum% -W %width% -H %height% -F %fps% -D %dcaps% -v 3 -P 1 --discard-after 0 --scan-every 0 --no-3d-adapt 1 --max-feature-updates 900
+facetracker -c %cameraNum% -F %fps% -D %dcaps% -v 3 -P 1 --discard-after 0 --scan-every 0 --no-3d-adapt 1 --max-feature-updates 900
 
 pause
